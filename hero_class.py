e0c9215ff17fecbd776723ab5cd110e404b4302a
attapun-an <attapunanivat@gmail.com>
2018-01-16 09:14:55 +0700

    first commit
    hi

---
+++ hero_class.py
@@ -0,0 +1,25 @@
+import pickle # library required to create binary files
+
+class Hero:
+
+    def __init__(self): # constructor
+        # self.heroID = ""
+        self.heroName = ""
+        self.age = 0
+        self.height = 0
+        self.weight = 0
+        self.teir = 0 # from 0-5
+
+# main menu
+    # if 0 exit
+
+    # elif 1 add a hero
+
+    # elif 2 display hero (by reading the file)
+
+# add hero
+
+
+
+
+
